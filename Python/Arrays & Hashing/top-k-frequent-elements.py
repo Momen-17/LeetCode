344c3030bf5c2b2a9b4693f91ba3f0738b8eeeb3
MomenAhmed <mo2mination@gmail.com>
2024-02-06 00:18:13 +0200

    Solve top k frequent elements with python

---
+++ Python/Arrays & Hashing/top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        nums_count = dict()
+        for num in nums:
+            nums_count[num] = nums_count.get(num, 0) + 1
+        
+        ret = sorted(nums_count.items(), key=lambda item: item[1], reverse=True)[:k]
+        return [x[0] for x in ret[:k]]
+
+
+print(Solution().topKFrequent([1,1,1,2,2,3], 2))
