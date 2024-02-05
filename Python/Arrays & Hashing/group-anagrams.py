4f3a22173024508b640fe1ac872baeab2c5582d6
MomenAhmed <mo2mination@gmail.com>
2024-02-05 04:59:53 +0200

    Solve group anagrams with python

---
+++ Python/Arrays & Hashing/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ret = {}
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            if sorted_string not in ret:
+                ret[sorted_string] = []
+            ret[sorted_string].append(string)
+        
+        return ret.values()
+
+
+print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
